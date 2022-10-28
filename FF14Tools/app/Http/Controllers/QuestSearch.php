1dc8cdde64fa630ca5aece3308446098591b6341
Mundanity Fan <i@mundanity.moe>
2022-10-28 12:36:27 +0800

    清除无用代码

--- FF14Tools/app/Http/Controllers/QuestSearch.php
+++ FF14Tools/app/Http/Controllers/QuestSearch.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class QuestSearch extends Controller
 {
-    //
+
+    function GetInfo($QuestID = '0'){
+        $response = Http::get('https://xivapi.com'.'/quest/'.$QuestID);
+        $data = $response->body();
+        $data = json_decode($data);
+        if(is_string($data))
+            return print('yes');
+
+        return print(phpinfo());
+//        return var_dump($data);
+//        return print($data);
+//        return response()->json([
+//            $data,
+//        ])->setEncodingOptions(JSON_PRETTY_PRINT);
+    }
+
+    function GetQuestList(){
+
+    }
 }
